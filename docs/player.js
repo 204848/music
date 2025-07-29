d0c2650a468550c73396466951c21e97f2058872
204848 <143565148+204848@users.noreply.github.com>
2025-07-29 21:06:56 +0800

    Update player.js

--- docs/player.js
+++ docs/player.js
@@ -1,4 +1,4 @@
-let media="https://204848.github.io/music/media/"
+let media="https://music.1357924680liu.dpdns.org/media/"
 
 // Cache references to DOM elements.
 let elms = ['track','artist', 'timer', 'duration','post', 'playBtn', 'pauseBtn', 'prevBtn', 'nextBtn', 'playlistBtn', 'postBtn', 'waveBtn', 'volumeBtn', 'progress', 'progressBar','waveCanvas', 'loading', 'playlist', 'list', 'volume', 'barEmpty', 'barFull', 'sliderBtn'];
